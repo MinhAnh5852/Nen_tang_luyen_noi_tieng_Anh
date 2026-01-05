ff9146653ba2bc2f2c0eafbfcc8a28ded12c3270
MinhAnh5852 <anhpnm5852@ut.edu.vn>
2026-01-05 22:00:18 +0700

    cap nhat AIcore

--- AESP-Microservice-Project/services/AI-core-service/main.py
+++ AESP-Microservice-Project/services/AI-core-service/main.py
@@ -10,11 +10,11 @@ load_dotenv()
 
 app = FastAPI()
 
-# 1. Tải model Whisper về máy (chỉ chạy lần đầu, hoàn toàn free)
+#Tải model Whisper về máy
 model_whisper = whisper.load_model("base") 
 
-# 2. Cấu hình Groq API (Dùng Llama 3 - Miễn phí)
-client_groq = Groq(api_key=os.getenv("GROQ_API_KEY"))
+#Cấu hình Groq API
+client_groq = Groq(api_key="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")
 
 @app.post("/ai-core/analyze")
 async def analyze_speaking(
@@ -26,13 +26,13 @@ async def analyze_speaking(
     # LƯU FILE TẠM
     temp_file = f"temp_{file.filename}"
     with open(temp_file, "wb") as f:
-        f.write(file.file.read())
+        f.write(await file.read())
 
-    # BƯỚC 1: CHUYỂN AUDIO SANG TEXT (Chạy trên máy bạn - FREE)
+    #CHUYỂN AUDIO SANG TEXT
     result = model_whisper.transcribe(temp_file)
     user_text = result['text']
 
-    # BƯỚC 2: GỬI TEXT SANG GROQ ĐỂ PHÂN TÍCH (FREE & NHANH)
+    #GỬI TEXT SANG GROQ ĐỂ PHÂN TÍCH 
     chat_completion = client_groq.chat.completions.create(
         messages=[
             {
@@ -41,19 +41,19 @@ async def analyze_speaking(
             },
             {"role": "user", "content": f"Hãy sửa lỗi và chấm điểm câu này: {user_text}"}
         ],
-        model="llama3-8b-8192", # Model Llama 3 cực mạnh của Meta
+        model="llama-3.1-8b-instant", # Model Llama 3 cực mạnh của Meta
     )
     
     # Giả định lấy kết quả từ AI
     ai_feedback = chat_completion.choices[0].message.content
 
-    # BƯỚC 3: LƯU VÀO DATABASE xdpm
+    #LƯU VÀO DATABASE xdpm
     new_session = models.Session(
         Learner_ID=learner_id,
         Topic_ID=topic_id,
         Transcript=user_text,
         AI_Grammar_Score=80, # Trích xuất từ ai_feedback
-        AI_Pronunciation_Score=75 # Bạn có thể dùng Librosa để tính thêm
+        AI_Pronunciation_Score=75 
     )
     db.add(new_session)
     db.commit()
