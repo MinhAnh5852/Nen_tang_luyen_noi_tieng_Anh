50dee96b235796adb3169d8fa53f65a8dacdbb98
MinhAnh5852 <anhpnm5852@ut.edu.vn>
2026-01-09 23:35:20 +0700

    cap nhat AIcore

--- AESP-Microservice-Project/services/AI-core-service/database.py
+++ AESP-Microservice-Project/services/AI-core-service/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/xdpm"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "mysql+mysqlconnector://root@localhost:3306/xdpm")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
